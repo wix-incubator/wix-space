406149f25ae9221c0728888dcaa313060dfeaa74
kobia <kobia@wix.com>
2024-02-26 16:05:10 +0200

    WixPatternsProvider

--- src/dashboard/pages/page.tsx
+++ src/dashboard/pages/page.tsx
@@ -1,15 +1,15 @@
 import { withDashboard } from '@wix/dashboard-react';
 import { WixDesignSystemProvider } from '@wix/design-system';
-import { CairoProvider } from '@wix/dashboard-components-alpha/provider';
+import { WixPatternsProvider } from '@wix/dashboard-components-alpha/provider';
 import React from 'react';
 import { SpaceshipsTable } from '../../components/SpaceshipsTable/SpaceshipsTable';
 
 export default withDashboard(() => {
   return (
     <WixDesignSystemProvider features={{ newColorsBranding: true }}>
-      <CairoProvider>
+      <WixPatternsProvider>
         <SpaceshipsTable />
-      </CairoProvider>
+      </WixPatternsProvider>
     </WixDesignSystemProvider>
   );
 });
